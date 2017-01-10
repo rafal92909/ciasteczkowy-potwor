327c93e6f9709887c414f12b0c2b100679a09665
Rafal <rafal92909@onet.eu>
2017-01-11 00:28:36 +0100

    ciasteczkowy potwor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ciasteczkowy-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  count: number = 0;
+  class: string = "";
+  flag: boolean = true;
+  onClick() {
+    let tmpFlag = this.flag;
+    this.flag = false;
+    if (tmpFlag) {
+      this.class =  "angrytext";
+      let that = this;
+      setTimeout(function() {
+        that.count += 1;    
+        that.class =  "";
+        that.flag =  true;;
+        console.log('koniec');
+      }, 1250);
+    }
+    
+
+  }
+
 }
